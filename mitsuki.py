24135b2fdc33d6359e06e08cf349a5378bde5dd8
coolcmyk <75294231+coolcmyk@users.noreply.github.com>
2024-01-24 06:24:28 +0700

    Update mitsuki.py

--- mitsuki.py
+++ mitsuki.py
@@ -71,5 +71,5 @@ if st.session_state.messages[-1]["role"] != "Mitsuki":
                 full_response += item
                 placeholder.markdown(full_response)
             placeholder.markdown(full_response)
-    message = {"role": "assistant", "content": full_response}
+    message = {"role": "Mitsuki", "content": full_response}
     st.session_state.messages.append(message)
