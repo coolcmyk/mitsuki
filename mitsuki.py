752eb57df314f060aaa4ba343838a4716bb7cff9
coolcmyk <75294231+coolcmyk@users.noreply.github.com>
2024-01-24 06:28:52 +0700

    Update mitsuki.py

--- mitsuki.py
+++ mitsuki.py
@@ -51,7 +51,7 @@ def generate_Mitsuki_response(prompt_input):
             string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
     output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
                            input={"prompt": f"{string_dialogue} {prompt_input} Mitsuki: ",
-                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":3})
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":100})
     return output
 
 # User-provided prompt
