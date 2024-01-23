99d282541dcb6378d44e215abe0d92a6501dc300
coolcmyk <75294231+coolcmyk@users.noreply.github.com>
2024-01-24 06:20:30 +0700

    Update mitsuki.py

--- mitsuki.py
+++ mitsuki.py
@@ -7,10 +7,15 @@ st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
-    replicate_api = st.text_input('Enter Replicate API token:', type='password')
-    if not (replicate_api.startswith('r8_') and len(replicate_api) == 40):
-        st.warning('Please enter your credentials!', icon='⚠️')
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     st.subheader('Models and parameters')
@@ -24,30 +29,47 @@ with st.sidebar:
     max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
 
 # Store LLM generated responses
-if "messages" not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "Mitsuki", "content": "How may I assist you today?"}]
 
 # Display or clear chat messages
 for message in st.session_state.messages:
-    st.write(message["content"])
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
 def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.session_state.messages = [{"role": "Mitsuki", "content": "How may I assist you today?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-if st.sidebar.button('Clear Chat History'):
-    clear_chat_history()
+# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
+def generate_Mitsuki_response(prompt_input):
+    string_dialogue = "You are Mitsuki. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Mitsuki'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Mitsuki: ",
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+    return output
 
 # User-provided prompt
-prompt = st.text_input('User Prompt', key='user_prompt')
-if st.button('Submit'):
+if prompt := st.chat_input(disabled=not replicate_api):
     st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
 
-# Generate a new response if the last message is not from the assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.spinner("Thinking..."):
-        response = replicate.run(llm,
-                                  input={"prompt": f"{prompt}\n\nMitsuki:",
-                                         "temperature": temperature, "top_p": top_p, "max_length": max_length,
-                                         "repetition_penalty": 1})
-    st.session_state.messages.append({"role": "assistant", "content": response})
-    st.write(response)
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "Mitsuki":
+    with st.chat_message("Mitsuki"):
+        with st.spinner("Thinking..."):
+            response = generate_Mitsuki_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
